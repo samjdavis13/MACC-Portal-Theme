c469386eb2b30238725cc0863573a018e3a1b069
Sam <samjd1993@gmail.com>
2014-06-10 13:20:25 +1000

    2 column footer

--- footer.php
+++ footer.php
@@ -2,24 +2,16 @@
       <div class='row'>
         <div class='large-12 column'>
 
-          <div class='large-4 column hide-for-medium hide-for-small'>
+          <div class='large-4 large-offset-2 columns hide-for-medium hide-for-small'>
             <img src="<?php bloginfo('template_directory'); ?>/img/logo-reverse.png">
           </div>
 
-          <div class='large-4 column'>
+          <div class='large-4 columns end'>
             <ul class='symbol'>
-              <li><a href="https://www.facebook.com/pages/Mount-Annan-Christian-College/89294472495?ref=br_tf">circlefacebook</a></li>
-              <li><a href="https://twitter.com/search?q=%23maccchat&src=typd">circletwitterbird</a></li>
-              <li><a href="mailto:admin@macc.nsw.edu.au">circleemail</a></li>
+              <li><a href="https://www.facebook.com/pages/Mount-Annan-Christian-College/89294472495?ref=br_tf" target="_blank">circlefacebook</a></li>
+              <li><a href="https://twitter.com/search?q=%23maccchat&src=typd" target="_blank">circletwitterbird</a></li>
+              <li><a href="mailto:admin@macc.nsw.edu.au" target="_blank">circleemail</a></li>
             </ul>
-          </div>
-
-          <div class='large-4 column hide-for-medium hide-for-small'>
-            <form method="get" action="http://www.google.com/search">
-              <input type='search' name="q" size="25" style="color:#808080;"
-maxlength="255" placeholder="Google Search">
-              <input type="submit" value="Go!" />
-            </form>
           </div>
 
         </div>
