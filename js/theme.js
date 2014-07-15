6be0ccfba44eabd53213e2cb5497038ca21d46ef
Sam <samjd1993@gmail.com>
2014-07-16 08:52:04 +1000

    Remove old code

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,3 @@
-/*
-window.onscroll = function() {
-  	// called when the window is scrolled.
-  	var windowHeight = document.pageYOffset;
-
-  	var searchBar = document.getElementById('googleSearchBar');
-  	if (windowHeight > 0) {
-    	searchBar.classList.add('scrolled');
-  	}
-	else {	
-    	searchBar.classList.remove('scrolled');
-	}
-};
-*/
-
 $(document).scroll(function(){
 	var windowHeight = $(document).scrollTop();
 	var searchBar = $("#googleSearchBar");
