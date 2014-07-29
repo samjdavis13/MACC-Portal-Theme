b67a6865dd7ecb2f2d9d1452d5dc93651c0f29ab
Sam <samjd1993@gmail.com>
2014-07-30 09:36:02 +1000

    Fix open in new tab bug

--- home-posts.php
+++ home-posts.php
@@ -17,7 +17,7 @@ Template Name: Home Posts
 				?>
 				<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 					<li>
-			        	<a href='<?php the_field( 'link' ) ?>'>
+			        	<a href='<?php the_field( 'link' ) ?>' target='_blank'>
 			            	<div class='item-container'>
 			            		<div class='large-4 columns'>
 				                  	<img src="<?php the_field( 'image' ); ?>">
